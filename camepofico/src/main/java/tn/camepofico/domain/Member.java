495a1a621ad964f4cd0d7a1bbcd9efaafad06671
LJH-test <ljhwpzm@naver.com>
2023-04-04 17:46:15 +0900

    jh commit

--- camepofico/src/main/java/tn/camepofico/domain/Member.java
+++ camepofico/src/main/java/tn/camepofico/domain/Member.java
@@ -1,4 +1,19 @@
 package tn.camepofico.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
+    private long mb_seq;
+    private String mb_name;
+    private String mb_email;
+    private String mb_password;
+    private Date mb_rdate;
+    private Date mb_udate;
 }
