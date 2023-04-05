50218eaa38b7b87e48fbc3e61c8175e844c540ca
LJH-test <ljhwpzm@naver.com>
2023-04-05 17:42:59 +0900

    회원가입함-진형

---
+++ camepofico/src/main/java/tn/camepofico/controller/MemberController.java
@@ -0,0 +1,52 @@
+package tn.camepofico.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tn.camepofico.domain.Member;
+import tn.camepofico.service.MemberService;
+
+import java.util.List;
+@RequestMapping("member/")
+@Controller
+public class MemberController {
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping("join")
+    public String join(){
+        return "join";
+    }
+    @PostMapping("join")
+    public String write(Member member){
+        memberService.insert(member);
+        return "main";
+    }
+    @GetMapping("list")
+    public String list(Model model){
+        List<Member> list = memberService.list();
+        model.addAttribute("list",list);
+        return "list";
+    }
+
+    @PostMapping("update")
+    public String updateOk(Member member){
+        memberService.update(member);
+        return "list";
+    }
+
+    @GetMapping("update")
+    public String update(Long mb_seq, Model model){
+        System.out.println("``````````````````````" + mb_seq);
+        Member member = memberService.findBySeq(mb_seq);
+        model.addAttribute("member", member);
+        return "join";
+    }
+    @GetMapping("delete")
+    public String delete(Long mb_seq){
+        return "list";
+    }
+}
---
+++ camepofico/src/main/java/tn/camepofico/service/MemberService.java
@@ -0,0 +1,12 @@
+package tn.camepofico.service;
+
+import tn.camepofico.domain.Member;
+
+import java.util.List;
+
+public interface MemberService {
+    void insert(Member member);
+    List<Member> list();
+    void update(Member member);
+    Member findBySeq(Long mb_seq);
+}
